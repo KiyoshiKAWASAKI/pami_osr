b16adcd3f3165426ba7f0230a8cdbbdc197d4835
Jin Huang <kiyoshi@Jins-MacBook-Pro-2019.local>
2020-08-09 19:18:54 -0400

    organizing the code

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ if args.use_valid:
 else:
     args.splits = ['train', 'val']
 
-if args.data == 'cifar10':
-    args.num_classes = 10
-elif args.data == 'cifar100':
-    args.num_classes = 100
-else:
-    args.num_classes = 413
+# if args.data == 'cifar10':
+#     args.num_classes = 10
+# elif args.data == 'cifar100':
+#     args.num_classes = 100
+# else:
+#     args.num_classes = 413
 
 import torch
 import torch.nn as nn
@@ -49,10 +49,6 @@ from timeit import default_timer as timer
 
 torch.manual_seed(args.seed)
 
-def take(n, iterable):
-    "Return first n items of the iterable as a list"
-    return list(islice(iterable, n))
-
 
 
 def main():
@@ -62,27 +58,7 @@ def main():
     if not os.path.exists(args.save):
         os.makedirs(args.save)
 
-    # Initialize a model with 5 blocks (original)
     model = getattr(models, args.arch)(args, nb_blocks=5)
-
-    # Get the block list
-    # parallel_blks = list(model.children())[0]
-    # clf_blks = list(model.children())[1]
-    #
-    # print(len(parallel_blks))
-    # print(len(clf_blks))
-
-    # Initialize a model with 1 block
-    # model_list_1 = parallel_blks[0].append(clf_blks[0])
-    # model_clf_1 = nn.Sequential(model_list_1)
-
-    # # Initialize a model with 2 blocks
-    # model_clf_2 = getattr(models, args.arch)(args, nb_blocks=2)
-    # # Initialize a model with 3 blocks
-    # model_clf_3 = getattr(models, args.arch)(args, nb_blocks=3)
-    # # Initialize a model with 4 blocks
-    # model_clf_4 = getattr(models, args.arch)(args, nb_blocks=4)
-
 
     if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
         model.features = torch.nn.DataParallel(model.features)
@@ -112,78 +88,10 @@ def main():
     if args.evalmode is not None:
         print("****************Doing testing only****************")
         state_dict = torch.load(args.evaluate_from)['state_dict']
-        # with open(os.path.join(args.save, "state_dict_info.csv"), 'w') as f:
-        #     writer = csv.writer(f, delimiter=',')
-        #
-        #     for key, value in state_dict.items():
-        #         print(key)
-        #         print(value.size())
-        #         # writer.writerow([key, value.size()])
-
-        # sys.exit()
-        # Original weights has 1127 layers with 5 blocks
-        # print(block_1+block_2+block_3+block_4+block_5+clfs)
-        # 361 294 216 138 48 70
-
-        # Get the weight keys for each block
-        # block_1, block_2, block_3, block_4 = [], [], [], []
-        # clf_1, clf_2, clf_3, clf_4 = [], [], [], []
-        #
-        # for key in state_dict.keys():
-        #     if key.startswith("module.blocks.0"):
-        #         block_1.append(key)
-        #     elif key.startswith("module.blocks.1"):
-        #         block_2.append(key)
-        #     elif key.startswith("module.blocks.2"):
-        #         block_3.append(key)
-        #     elif key.startswith("module.blocks.3"):
-        #         block_4.append(key)
-        #
-        #     elif key.startswith("module.classifier.0"):
-        #         clf_1.append(key)
-        #     elif key.startswith("module.classifier.1"):
-        #         clf_2.append(key)
-        #     elif key.startswith("module.classifier.2"):
-        #         clf_3.append(key)
-        #     elif key.startswith("module.classifier.3"):
-        #         clf_4.append(key)
-
-
-        # Get different weights for 5 models perspectively
-        # state_dict_1_key = block_1 + clf_1
-        # state_dict_2_key = state_dict_1_key + block_2 + clf_2
-        # state_dict_3_key = state_dict_2_key + block_3 + clf_4
-        # state_dict_4_key = state_dict_3_key + block_4 + clf_4
-
-        # print(len(state_dict_1_key)) # 375
-
-        # Get the weights according to keys
-        # state_dict_1 = {k: state_dict[k] for k in state_dict_1_key}
-        # state_dict_2 = {k: state_dict[k] for k in state_dict_2_key}
-        # state_dict_3 = {k: state_dict[k] for k in state_dict_3_key}
-        # state_dict_4 = {k: state_dict[k] for k in state_dict_4_key}
-
-        # state_dict_1_new = {key[7:]: value for key, value in state_dict_1.items()}
-        # print(state_dict_1_new.keys())
-
-        # state_dict_2_new = {key[7:]: value for key, value in state_dict_2.items()}
-        # state_dict_3_new = {key[7:]: value for key, value in state_dict_3.items()}
-        # state_dict_4_new = {key[7:]: value for key, value in state_dict_4.items()}
-
-
-        # Load weights
-        # model_clf_1.load_state_dict(state_dict_1_new)
-        # model_clf_2.load_state_dict(state_dict_2_new)
-        # model_clf_3.load_state_dict(state_dict_3_new)
-        # model_clf_4.load_state_dict(state_dict_4_new)
         model.load_state_dict(state_dict)
 
-        print("Finished loading weights for all 5 models.")
-
-
-        # "anytime" is the one with early exits and the one we want
         if args.evalmode == 'anytime':
-            if args.test_with_novel:
+            if args.train_k_plus_1 or args.test_with_novel:
                 test_with_novelty(val_loader=test_loader,
                                   model=model,
                                   criterion=criterion)
@@ -191,18 +99,17 @@ def main():
                 validate(test_loader, model, criterion)
 
         else:
-            dynamic_evaluate(model, test_loader, val_loader, args)
+            print("Only supporting anytime prediction!")
+
         return
 
-    scores = ['epoch\tlr\ttrain_loss\tval_loss\ttrain_prec1'
-              '\tval_prec1\ttrain_prec5\tval_prec5']
+    scores = ['epoch\tlr\ttrain_loss\tval_loss\ttrain_prec1\tval_prec1\ttrain_prec5\tval_prec5']
 
     # Here is for training and validation
     for epoch in range(args.start_epoch, args.epochs):
         # Adding the option for training k+1 classes
         if args.train_k_plus_1 == True:
             print("!! Training MSD-Net on K+1 classes.")
-
             train_loss, train_prec1, train_prec3, train_prec5, lr = train_k_plus_one(train_loader, model, criterion, optimizer, epoch)
             val_loss, val_prec1, val_prec3, val_prec5 = validate_k_plus_one(val_loader, model, criterion, epoch)
 
@@ -213,10 +120,9 @@ def main():
             train_loss, train_prec1, train_prec5, lr = train(train_loader, model, criterion, optimizer, epoch)
             val_loss, val_prec1, val_prec5 = validate(val_loader, model, criterion, epoch)
 
-        scores.append(('{}\t{:.3f}' + '\t{:.4f}' * 6)
-                      .format(epoch, lr, train_loss, val_loss,
-                              train_prec1, val_prec1, train_prec5, val_prec5))
+        scores.append(('{}\t{:.3f}' + '\t{:.4f}' * 6).format(epoch, lr, train_loss, val_loss, train_prec1, val_prec1, train_prec5, val_prec5))
 
+        # Find the best model
         is_best = val_prec1 > best_prec1
         if is_best:
             best_prec1 = val_prec1
@@ -229,13 +135,9 @@ def main():
             'arch': args.arch,
             'state_dict': model.state_dict(),
             'best_prec1': best_prec1,
-            'optimizer': optimizer.state_dict(),
-        }, args, is_best, model_filename, scores)
+            'optimizer': optimizer.state_dict(),}, args, is_best, model_filename, scores)
 
     print('Best val_prec1: {:.4f} at epoch {}'.format(best_prec1, best_epoch))
-
-    # print('********** Final prediction results **********')
-    # validate(test_loader, model, criterion)
 
     return
 
@@ -244,6 +146,7 @@ def main():
 
 def train(train_loader, model, criterion, optimizer, epoch):
     """
+    Original training function from the authors.
 
     :param train_loader:
     :param model:
@@ -256,6 +159,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
+
     # TODO: Update the evaluation metrics to top-1, top-3 and top-5
     top1, top5 = [], []
     for i in range(args.nBlocks):
@@ -338,6 +242,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
 def train_k_plus_one(train_loader, model, criterion, optimizer, epoch):
     """
+    Training k known classes and all other classes as +1 unknown class.
 
     :param train_loader:
     :param model:
@@ -350,7 +255,8 @@ def train_k_plus_one(train_loader, model, criterion, optimizer, epoch):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
-    # TODO: Update the evaluation metrics to top-1, top-3 and top-5
+
+    # Update the evaluation metrics to top-1, top-3 and top-5
     top1, top3, top5 = [], [], []
     for i in range(args.nBlocks):
         top1.append(AverageMeter())
@@ -434,169 +340,6 @@ def train_k_plus_one(train_loader, model, criterion, optimizer, epoch):
 
     return losses.avg, top1[-1].avg, top3[-1].avg, top5[-1].avg, running_lr
 
-
-
-
-def train_k_plus_one_hold(train_loader, model, criterion, optimizer, epoch):
-    """
-    Train the model using k known classes and all other classes as +1 class (aka unknown)
-
-    :param train_loader:
-    :param model:
-    :param criterion:
-    :param optimizer:
-    :param epoch:
-    :return:
-    """
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-
-
-    model.train()
-
-    end = time.time()
-
-    running_lr = None
-
-    with open(os.path.join(args.save, "training_stats_epoch_" + str(epoch) + ".txt"), 'w') as train_f:
-        for i, (input, target) in enumerate(train_loader):
-            lr = adjust_learning_rate(optimizer, epoch, args, batch=i,
-                                      nBatch=len(train_loader), method=args.lr_type)
-
-
-            if running_lr is None:
-                running_lr = lr
-
-            data_time.update(time.time() - end)
-
-            target = target.cuda(async=True)
-
-
-            # Check the labels, change the label if it belongs to "unknown"
-            # Be reminded that the label generated by data loader starts from 0
-            for k in range(len(target)):
-                if target[k] >= args.nb_training_classes-1 :
-                    target[k] = args.nb_training_classes-1
-
-            # print(target)
-
-            input_var = torch.autograd.Variable(input)
-            target_var = torch.autograd.Variable(target)
-
-            output = model(input_var)
-
-            if not isinstance(output, list):
-                output = [output]
-
-            loss = 0.0
-            for j in range(len(output)):
-                loss += criterion(output[j], target_var)
-
-            losses.update(loss.item(), input.size(0))
-
-            # compute gradient and do SGD step
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-
-            # TODO: separate the evaluation for known and unknown
-            # Check whether there is unknown:
-            # If the unknown exists, calculate both separately
-            if (args.nb_training_classes-1) in target:
-                top1_known, top3_known, top5_known = [], [], []
-                top1_unknown, top3_unknown, top5_unknown = [], [], []
-
-                for i in range(args.nBlocks):
-                    top1_known.append(AverageMeter())
-                    top3_known.append(AverageMeter())
-                    top5_known.append(AverageMeter())
-                    top1_unknown.append(AverageMeter())
-                    top3_unknown.append(AverageMeter())
-                    top5_unknown.append(AverageMeter())
-
-                out_put_known = []
-                target_known = []
-
-                out_put_unknown = []
-                target_unknown = []
-
-                # Separate the known and unknown output
-                for i, label in enumerate(target):
-                    if (label == args.nb_training_classes-1):
-                        out_put_unknown.append(output[i])
-                        target_unknown.append(target[i])
-                    else:
-                        out_put_known.append(output[i])
-                        target_known.append(target[i])
-
-                # Calculate acc for known
-                for j in range(len(out_put_known)):
-                    prec1_known, prec3_known, prec5_known = accuracy(out_put_known[j].data,
-                                                                     torch.autograd.Variable(target_known),
-                                                                     topk=(1, 3, 5))
-
-                    top1_known[j].update(prec1_known.item(), input.size(0))
-                    top3_known[j].update(prec3_known.item(), input.size(0))
-                    top5_known[j].update(prec5_known.item(), input.size(0))
-
-                # Calculate acc for unknown
-                for k in range(len(out_put_unknown)):
-                    prec1_known, prec3_known, prec5_known = accuracy(out_put_unknown[k].data,
-                                                                     torch.autograd.Variable(target_unknown),
-                                                                     topk=(1, 3, 5))
-
-                    top1_known[k].update(prec1_known.item(), input.size(0))
-                    top3_known[k].update(prec3_known.item(), input.size(0))
-                    top5_known[k].update(prec5_known.item(), input.size(0))
-
-
-                return losses.avg, top1_known[-1].avg, top3_known[-1].avg, top3_known[-1].avg, \
-                       top1_unknown[-1].avg, top3_unknown[-1].avg, top5_known[-1].avg, running_lr
-
-
-            # Otherwise, only compute the acc for known samples
-            else:
-                top1, top3, top5 = [], [], []
-                for i in range(args.nBlocks):
-                    top1.append(AverageMeter())
-                    top3.append(AverageMeter())
-                    top5.append(AverageMeter())
-
-                for j in range(len(output)):
-                    prec1, prec3, prec5 = accuracy(output[j].data, target, topk=(1, 3, 5))
-                    top1[j].update(prec1.item(), input.size(0))
-                    top3[j].update(prec3.item(), input.size(0))
-                    top5[j].update(prec5.item(), input.size(0))
-
-                # if i % args.print_freq == 0:
-                #     print('Epoch: [{0}][{1}/{2}]\t'
-                #           'Time {batch_time.avg:.3f}\t'
-                #           'Data {data_time.avg:.3f}\t'
-                #           'Loss {loss.val:.4f}\t'
-                #           'Known-Acc@1 {top1.val:.4f}\t'
-                #           'Known-Acc@3 {top3.val:.4f}\t'
-                #           'Known@5 {top5.val:.4f}'.format(
-                #         epoch, i + 1, len(train_loader),
-                #         batch_time=batch_time, data_time=data_time,
-                #         loss=losses, top1=top1[-1], top3=top3[-1], top5=top5[-1]))
-                #
-                #     train_f.write('Epoch: [{0}][{1}/{2}]\t'
-                #                   'Time {batch_time.avg:.3f}\t'
-                #                   'Data {data_time.avg:.3f}\t'
-                #                   'Loss {loss.val:.4f}\t'
-                #                   'Known-Acc@1 {top1.val:.4f}\t'
-                #                   'Known-Acc@3 {top3.val:.4f}\t'
-                #                   'Known-Acc@5 {top5.val:.4f}\n'.format(
-                #         epoch, i + 1, len(train_loader),
-                #         batch_time=batch_time, data_time=data_time,
-                #         loss=losses, top1=top1[-1], top3=top3[-1], top5=top5[-1]))
-
-                return losses.avg, top1[-1].avg, top5[-1].avg, top3[-1].avg, running_lr
 
 
 
@@ -1024,6 +767,13 @@ def adjust_learning_rate(optimizer, epoch, args, batch=None,
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     return lr
+
+
+
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
 
 if __name__ == '__main__':
     main()
