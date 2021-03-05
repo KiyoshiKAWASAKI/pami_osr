9f1e3e83e263309250ca6df3089c7081ef368cd8
prijatelj <theomnipotentwoof@gmail.com>
2021-03-05 17:52:20 -0500

    update before leaving lab

--- dsp/data.py
+++ dsp/data.py
@@ -110,24 +110,15 @@ def process_raw_csv(
     start_row=0,
     output_path=None,
     worker_id_col='AssignmentId',
+    rm_top_rt_percent=0.0,
+    rm_control=True,
 ):
     """Process the raw CSV version of the Amazon Turk annotators, but preserve
     some information about the annotator, such as their performance on control
     questions.
 
-    This should output to similar format as existing once modified, but also
-    save the following:
-        - Annotator Confusion Tensor with shape [Classes, Classes, Annotators,
-          Ordering], where ordering is the class as known and class as unknown.
-          Flattening on the ordering dim is probably the end result.
-        - Annotator Reaction Time Tensor with shape [Classes, Classes,
-          Annotators, Ordering].
-        - Annotator Control Question Accuracy
-
-    Ideally, all the above w/ annotators will order the annotators by their
-    control question accuracy first, then date time by occurrence.
-
-    [Args]
+    Args
+    ----
     raw_csv_path : str
         Input filepath for the raw, unprocessed csv of experiment
         annotations
@@ -170,11 +161,14 @@ def process_raw_csv(
         ]
     elif os.path.splitext(control_filepath)[1] == 'yaml':
         # TODO
+        raise NotImplementedError()
     else:
         raise ValueError('Expected filetype of npy or yaml')
 
     # Get rid of the "[" and "]" in the image list
     raw_data['ImageList'] = raw_data['ImageList'].apply(lambda x: x[1:-1])
+
+    control_scores = np.zeros([worker_ids.shape[0], len(control_img_list)])
 
     # Check the data for each worker
     for worker_id in worker_ids:
@@ -190,10 +184,19 @@ def process_raw_csv(
         # responses.
 
         # The number of responses should be 25, but we allow 2 more entries
-        if nb_responses >= 27:
+        if nb_responses >= 27 or nb_responses < 25:
+            if nb_responses >= 27:
+                logging.info(
+                    'Annotator: `%s` removed for having 27 or more answers.'
+                )
+            elif nb_responses < 25:
+                logging.info(
+                    'Annotator: `%s` removed for having less than 25 answers.'
+                )
             raw_data = raw_data.drop(
                 raw_data[raw_data[worker_id_col] == worker_id].index
             )
+            continue
 
         # Drop the rows from the workers who answred more than 2 control
         # questions wrong
@@ -211,6 +214,8 @@ def process_raw_csv(
             if image_list_formatted in control_img_list:
                 if question.ImposterFound == 0:
                     nb_control_wrong += 1
+
+                    # TODO here add which it is to update table
 
                 # No matter what, drop this line of record (cause it is a
                 # control question)
@@ -233,7 +238,7 @@ def process_raw_csv(
     raw_data.reset_index(drop=True).to_csv(path_or_buf=processed_csv_save_path)
 
 
-def annotator_control_question_score():
+def annotator_control_question_score(
     csv_path,
     control_filepath,
     output_path,
@@ -244,7 +249,29 @@ def annotator_control_question_score():
     """Given the annotation csv and control questions, calculates each
     annotator's control question score, which is an integer between [0, 5].
     """
-    # TODO load csv
+    # Load annotation csv
+    data = pd.read_csv(csv_path)
+
+    # Drop initial rows being skipped
+    if start_row > 0:
+        data = data.iloc[start_row:]
+
+    logging.info('There are %d entries (rows) to be processed.', data)
+
+    # TODO save csv of annotator control question performance
+    worker_ids = raw_data[worker_id_col].unique()
+
+    # TODO save "csv"/tensor of individual annotator confusion
+
+    # TODO save csv of macro mean annotator class confusion
+    # TODO save "csv"/tensor of macro mean annotator class RT
+    #   stat summary: mean, sd, median, quantiles, min, max
+
+
+
+
+    # Get rid of the "[" and "]" in the image list
+    data['ImageList'] = data['ImageList'].apply(lambda x: x[1:-1])
 
     # TODO load control questions
 
@@ -267,6 +294,19 @@ def annotator_confusion_matrices():
 ):
     """Given the annotation csv and control questions, calculates each
     annotator's control question score, which is an integer between [0, 5].
+
+    This should output to similar format as existing once modified, but also
+    save the following:
+        - Annotator Confusion Tensor with shape [Classes, Classes, Annotators,
+          Ordering], where ordering is the class as known and class as unknown.
+          Flattening on the ordering dim is probably the end result.
+        - Annotator Reaction Time Tensor with shape [Classes, Classes,
+          Annotators, Ordering].
+        - Annotator Control Question Accuracy
+
+    Ideally, all the above w/ annotators will order the annotators by their
+    control question accuracy first, then date time by occurrence.
+
     """
     raise NotImplementedError()
 
